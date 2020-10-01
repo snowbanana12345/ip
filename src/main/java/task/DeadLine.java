afefbb7973a3f9b220f4f1606ddae0848e5354d9
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-10-01 13:29:29 +0800

    did the same for deadline

--- src/main/java/task/DeadLine.java
+++ src/main/java/task/DeadLine.java
@@ -1,16 +1,50 @@
 package task;
 
+import duke_exception.BadInputException;
+
 public class DeadLine extends Task {
     private final String deadline;
+    private boolean hasValidDateTime;
+    private DateTime dateTime;
     public DeadLine(String task, String deadline){
         super(task);
         this.deadline = deadline;
+        this.hasValidDateTime = false;
+        this.dateTime = new DateTime(0,0,0,0);
+
+        try {
+            this.dateTime = DateTimeParser.parseDateTime(deadline);
+            this.hasValidDateTime = true;
+        }
+        catch (BadInputException e){
+            System.out.println(e);
+        }
     }
+
     public String getBy(){
         return this.deadline;
     }
+
+    public boolean isOnDate(String dateTime) throws BadInputException {
+        if (!(hasValidDateTime)){
+            return false;
+        }
+        try {
+            DateTime date = DateTimeParser.parseDateTime(dateTime);
+            return DateTime.isSameDate(this.dateTime, date);
+        }
+        catch (BadInputException e){
+            throw new BadInputException("The date time entered has a problem:" + e.getMessage());
+        }
+    }
+    
     @Override
     public String toString(){
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        if (hasValidDateTime){
+            return "[E]" + super.toString() + "(at: " + this.dateTime + ")";
+        }
+        else {
+            return "[E]" + super.toString() + "(at: " + this.deadline + ")";
+        }
     }
 }
